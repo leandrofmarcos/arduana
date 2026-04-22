41bb784d880e016c4feaaa2ac00a413c4b24383b
Developer <dev@prototipos.com>
2026-04-22 17:15:48 -0300

    fix: corrige role do usuario admin de Despachante para Administrador

---
+++ comex133_api/Migrations/20260422201351_FixAdminUserRole.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comex133Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAdminUserRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Usuário admin@comex133.com.br (id=1) estava vinculado a Despachante (id=1).
+            // Deve ser vinculado a Administrador (id=4).
+            migrationBuilder.Sql(@"
+                -- Remove vínculo incorreto com Despachante
+                DELETE FROM [UsuarioRoles] WHERE [UsuarioId] = 1 AND [RoleId] = 1;
+
+                -- Garante vínculo correto com Administrador (evita duplicata)
+                IF NOT EXISTS (SELECT 1 FROM [UsuarioRoles] WHERE [UsuarioId] = 1 AND [RoleId] = 4)
+                    INSERT INTO [UsuarioRoles] ([UsuarioId], [RoleId], [AtribuidoEm]) VALUES (1, 4, GETDATE());
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM [UsuarioRoles] WHERE [UsuarioId] = 1 AND [RoleId] = 4;
+
+                IF NOT EXISTS (SELECT 1 FROM [UsuarioRoles] WHERE [UsuarioId] = 1 AND [RoleId] = 1)
+                    INSERT INTO [UsuarioRoles] ([UsuarioId], [RoleId], [AtribuidoEm]) VALUES (1, 1, GETDATE());
+            ");
+        }
+    }
+}
